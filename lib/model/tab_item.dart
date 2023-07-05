b602dc57680c3d697c6a3aef5028bc95f2e7def0
Anas Fikhi <work@gwhyyy.com>
2023-07-05 19:35:26 +0100

    [ Edit ] commented and refactored the tab item model

--- lib/model/tab_item.dart
+++ lib/model/tab_item.dart
@@ -1,9 +1,17 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
 
+/// {@template tab_item}
+/// A model class that represents data relted to a tabItem to be used with a [Tab] & [TabBarView].
+/// {@endtemplate}
 class TabItem extends Equatable {
+  /// The widget associated with this tab to be shown.
   final Widget widget;
+
+  /// THe label of this tab to be shown in the [Tab].
   final String label;
+
+  /// The icon to be represented in the [Tab].
   final IconData icon;
 
   @override
@@ -13,6 +21,7 @@ class TabItem extends Equatable {
         icon,
       ];
 
+  /// {@macro tab_item}
   const TabItem({
     required this.widget,
     required this.label,
