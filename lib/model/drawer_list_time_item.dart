bfad86ec248c593f66994631018f5673856667e6
Anas Fikhi <work@gwhyyy.com>
2023-07-05 18:55:01 +0100

    [ Edit ] commented and refactored the drawer list item model

--- lib/model/drawer_list_time_item.dart
+++ lib/model/drawer_list_time_item.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
+/// {@template drawer_list_time_item}
+/// A model class that holds data related to a drawer list item.
+/// {@endtemplate}
 @immutable
 class DrawerListTimeItem {
+  /// The icon to be shown in the tile of the drawer item.
   final IconData icon;
+
+  /// A label/name of this item
   final String label;
+
+  /// A callback that will run when the user taps on the drawer item.
   final void Function() onTap;
+
+  /// Weither a user is authenticated or logs out.
   final bool isLogout;
 
+  /// {@macro drawer_list_time_item}
   const DrawerListTimeItem({
     required this.icon,
     required this.label,
