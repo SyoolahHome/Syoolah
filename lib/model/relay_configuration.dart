b9f49fa9f2bd848d8c4106cfba14c3ad4f5ab405
Anas Fikhi <work@gwhyyy.com>
2023-07-05 19:18:36 +0100

    [ Edit ] commented and refactored the report option model

--- lib/model/relay_configuration.dart
+++ lib/model/relay_configuration.dart
@@ -1,17 +1,27 @@
 import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
 
+/// {@template relay_config}
+/// A model class that represents a relay configuration
+/// {@endtemplate}
+@immutable
 class RelayConfiguration extends Equatable {
+  /// The web socket url of this Nostr relay.
   final String url;
+
+  /// Weither this relay is activated for Nostr operations.
   final bool isActive;
 
   @override
   List<Object?> get props => [url, isActive];
 
+  /// {@macro relay_config}
   const RelayConfiguration({
     required this.url,
     this.isActive = true,
   });
 
+  /// {@macro relay_config}
   RelayConfiguration copyWith({
     String? url,
     bool? isActive,
@@ -19,6 +29,16 @@ class RelayConfiguration extends Equatable {
     return RelayConfiguration(
       url: url ?? this.url,
       isActive: isActive ?? this.isActive,
+    );
+  }
+
+  /// {@macro relay_config}
+  factory RelayConfiguration.active({
+    required String url,
+  }) {
+    return RelayConfiguration(
+      url: url,
+      isActive: true,
     );
   }
 }
--- lib/model/report_option.dart
+++ lib/model/report_option.dart
@@ -1,25 +1,45 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
 
 import '../constants/app_enums.dart';
 
+/// {@template report_option}
+/// A model class that holds a Nostr notes report  types.
+/// {@endtemplate}
+
+@immutable
 class ReportOption extends Equatable {
+  /// The type of the report.
+  final ReportType reportType;
+
+  /// Weither this report option is selected by the user or not.
+  final bool isSelected;
+
+  /// {@macro report_option}
   ReportOption({
     this.isSelected = false,
     required this.reportType,
   });
 
-  final ReportType reportType;
-  final bool isSelected;
-
   @override
   List<Object?> get props => [reportType, isSelected];
 
+  /// {@macro report_option}
   ReportOption copyWithIsSelected({
     bool? isSelected,
   }) {
+    return copyWith(isSelected: isSelected);
+  }
+
+  /// {@macro report_option}
+  ReportOption copyWith({
+    ReportType? reportType,
+    bool? isSelected,
+  }) {
     return ReportOption(
+      reportType: reportType ?? this.reportType,
       isSelected: isSelected ?? this.isSelected,
-      reportType: reportType,
     );
   }
 }
