e49440a1b6c7ea6db4e6f8c2baa6b769540a498e
Anas Fikhi <work@gwhyyy.com>
2023-07-05 19:04:04 +0100

    [ Edit ] commented and refactored the local item model

--- lib/model/loclal_item.dart
+++ lib/model/loclal_item.dart
@@ -1,11 +1,22 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
+/// {@template local_item}
+/// A model class representing a lang locale of M.
+/// {@endtemplate}
+@immutable
 class LocaleItem extends Equatable {
+  /// The text of the buttpn to shown when moving to this local item in M.
   final String applyText;
+
+  /// The locale object that will be passed through the app code to activate it for the user.
   final Locale locale;
+
+  /// The name of the locale to be shown
   final String titleName;
 
+  /// {@macro local_item}
   const LocaleItem({
     required this.applyText,
     required this.locale,
@@ -18,4 +29,17 @@ class LocaleItem extends Equatable {
         locale,
         titleName,
       ];
+
+  /// {@macro local_item}
+  LocaleItem copyWith({
+    String? applyText,
+    Locale? locale,
+    String? titleName,
+  }) {
+    return LocaleItem(
+      applyText: applyText ?? this.applyText,
+      locale: locale ?? this.locale,
+      titleName: titleName ?? this.titleName,
+    );
+  }
 }
