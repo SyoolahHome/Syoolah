d81c2618ef9ecab65e1bf44229d9f85a9e888f61
Anas Fikhi <work@gwhyyy.com>
2023-07-05 16:43:21 +0100

    [ Edit ] commented and refactored the chat module  model

--- lib/model/chat_modules.dart
+++ lib/model/chat_modules.dart
@@ -1,13 +1,27 @@
+import 'package:dart_openai/openai.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
+/// {@template chat_module_item}
+/// A model class representing one single Imam chat module to presented as an Imam level for the end user.
+/// {@endtemplate}
 class ChatModuleItem extends Equatable {
+  /// The title of the chat module
   final String title;
+
+  /// A description for this chat module.
   final String subtitle;
+
+  /// An isonc for this chat modeule.
   final IconData icon;
+
+  /// An AI instruction that used to instruct the IMma initialy, basically to be used as a [OpenAIChatMessageRole.assistant] or [OpenAIChatMessageRole.system] on low level API to [dart_openai] package.
   final String instruction;
+
+  /// A list containing recommended question to be shown randomly or in an order way for the user as recommendations to start the chat experience.
   final List<String> recommendedQuestions;
 
+  /// {@macro chat_module_item}
   const ChatModuleItem({
     required this.title,
     required this.subtitle,
@@ -25,6 +39,8 @@ class ChatModuleItem extends Equatable {
         recommendedQuestions,
       ];
 
+  /// {@macro chat_module_item}
+  /// This specifically represent an beginner level Imam.
   factory ChatModuleItem.beginner({
     required IconData icon,
     required String instruction,
@@ -40,6 +56,8 @@ class ChatModuleItem extends Equatable {
     );
   }
 
+  /// {@macro chat_module_item}
+  /// This specifically represent an intermediate level Imam.
   factory ChatModuleItem.intermediate({
     required IconData icon,
     required String instruction,
@@ -54,6 +72,9 @@ class ChatModuleItem extends Equatable {
       subtitle: subtitle,
     );
   }
+
+  /// {@macro chat_module_item}
+  /// This specifically represent an advanced level Imam.
   factory ChatModuleItem.advanced({
     required IconData icon,
     required String instruction,
