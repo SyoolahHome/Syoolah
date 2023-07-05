289f84165e27781764ad13e7c2474302db477737
Anas Fikhi <work@gwhyyy.com>
2023-07-05 19:26:02 +0100

    [ Edit ] commented and refactored the settings item model

--- lib/model/search_option.dart
+++ lib/model/search_option.dart
@@ -1,12 +1,27 @@
 import 'package:ditto/model/note.dart';
 import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
 
+/// {@template search_option}
+/// A model class that holds data related to a single search option.
+/// {@endtemplate}
+@immutable
 class SearchOption extends Equatable {
+  /// The name of the search option.
   final String name;
+
+  /// Weither this option will a query to perform search.
   final bool useSearchQuery;
+
+  /// Weither this osearc option is activated/selected or not.
   final bool isSelected;
+
+  /// The function to perform the search function.
   final List<Note> Function(List<Note> feedPosts, String search) searchFunction;
+
+  /// Weither this option will manipulate an existing list of notes.
   final bool manipulatesExistingResultsList;
+
   @override
   List<Object?> get props => [
         name,
@@ -16,6 +31,7 @@ class SearchOption extends Equatable {
         manipulatesExistingResultsList,
       ];
 
+  /// {@macro search_option}
   const SearchOption({
     required this.name,
     required this.isSelected,
@@ -24,6 +40,7 @@ class SearchOption extends Equatable {
     this.manipulatesExistingResultsList = false,
   });
 
+  /// {@macro search_option}
   SearchOption copyWith({
     String? name,
     bool? useSearchQuery,
--- lib/model/settings_item.dart
+++ lib/model/settings_item.dart
@@ -1,12 +1,24 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
+/// {@template setting_item}
+/// A model class tat represents a setting item to be presented in the settings UI.
+/// {@endtemplate}
+@immutable
 class SettingsItem extends Equatable {
+  /// The icon to be shown for the settings item tile.
   final IconData icon;
+
+  /// The name/title of this setting.
   final String name;
-  final Function onTap;
+
+  /// THe callback to run when the user will tap on the setting tile.
+  final VoidCallback onTap;
+
+  /// The trailing to be adde to the setting tile if it eists.
   final Widget? trailing;
 
+  /// {@macro setting_item}
   const SettingsItem({
     required this.icon,
     required this.name,
