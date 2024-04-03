fd271059c9abce8764938b82b793cdd8f1f0fe78
Anas Fikhi <work@gwhyyy.com>
2024-04-03 17:51:26 +0000

    [ Edit ] set rounadabout about screen

--- lib/presentation/about_app/widgets/about_app_content.dart
+++ lib/presentation/about_app/widgets/about_app_content.dart
@@ -17,68 +17,68 @@ class AboutRoundaboutContent extends StatelessWidget {
 
     const height = 10.0;
 
+    final sections = <(String, String)>[
+      (
+        'Restoring Digital Liberty',
+        'Roundabout is a powerful antidote to censorship on the Internet and embodies the true spirit of liberty in the digital age.'
+      ),
+      (
+        'Reviving Social Media’s Original Vision',
+        'Roundabout returns to the essence of social media as a platform for free expression and connection, countering the trend of centralized control and censorship.'
+      ),
+      (
+        'Challenging the Status Quo',
+        'Roundabout not only changes how we interact online but also questions the underlying power structures of the digital world, advocating for a more balanced and equitable information ecosystem.'
+      ),
+      (
+        'Empowerment Through Decentralization',
+        'By decentralizing control, Roundabout hands back power to users, allowing them to shape their digital experience without interference.'
+      ),
+      (
+        'Innovating Beyond Social Media',
+        'Roundabout\'s potential extends far beyond traditional social media, offering a resilient framework for exciting new features like Reddit-style communities, Bitcoin payments for truly global banking and even Bitcoin ballots for elections hat can\'t be stolen.'
+      ),
+      (
+        'A New Era of Digital Interaction',
+        'Roundabout isn’t just a platform - it’s a movement towards a more open, interconnected digital world where freedom of expression and innovation thrive.',
+      ),
+    ];
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Animate(
-          effects: <Effect>[
-            FadeEffect(),
-            SlideEffect(
-              begin: Offset(0, 0.5),
-            ),
-          ],
-          delay: 400.ms,
-          child: HeadTitle(
-            title: "roundaboutAppAboutTitle".tr(),
-            isForSection: true,
-            minimizeFontSizeBy: 8,
-          ),
-        ),
         SizedBox(height: height),
-        ..."roundaboutAppAboutContent".tr().split("\n").indexedMap(
-              (index, item) => Animate(
-                  delay: (600 + index * 100).ms,
-                  effects: <Effect>[
-                    FadeEffect(),
-                    SlideEffect(
-                      begin: Offset(0, 0.5),
-                    ),
-                  ],
-                  child: Text(
-                    item + "\n",
-                  )),
-            ),
+        ...sections.indexed.map(
+          (entry) {
+            return Animate(
+              delay: (400 + entry.$1 * 100).ms,
+              effects: <Effect>[
+                FadeEffect(),
+                SlideEffect(
+                  begin: Offset(0, 0.5),
+                ),
+              ],
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    entry.$2.$1,
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleMedium
+                        ?.copyWith(fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: height),
+                  Text(
+                    entry.$2.$2,
+                  ),
+                  SizedBox(height: height * 3),
+                ],
+              ),
+            );
+          },
+        ),
         SizedBox(height: height * 2),
-        // if (!showOnlyAppDescription) ...[
-        //   Animate(
-        //     effects: <Effect>[
-        //       FadeEffect(),
-        //       SlideEffect(
-        //         begin: Offset(0, 0.5),
-        //       ),
-        //     ],
-        //     delay: 1000.ms,
-        //     child: HeadTitle(
-        //       title: "roundaboutNameTitle".tr(),
-        //       isForSection: true,
-        //       minimizeFontSizeBy: 8,
-        //     ),
-        //   ),
-        //   SizedBox(height: height),
-        //   ..."roundaboutNameAboutContent".tr().split("\n").indexedMap(
-        //         (index, item) => Animate(
-        //             delay: (1400 + index * 100).ms,
-        //             effects: <Effect>[
-        //               FadeEffect(),
-        //               SlideEffect(
-        //                 begin: Offset(0, 0.5),
-        //               ),
-        //             ],
-        //             child: Text(
-        //               item + "\n",
-        //             )),
-        //       )
-        // ],
       ],
     );
   }
